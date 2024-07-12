3dede04718dc78ff3ea857abd8b649a1de91bcdc
WenxiaoCai <robincwx@foxmail.com>
2024-07-12 19:55:09 +0800

    Update SpatialBench eval

--- script/eval/lora/spatial_bench.sh
+++ script/eval/lora/spatial_bench.sh
@@ -4,11 +4,11 @@ MODEL_TYPE=phi-2
 MODEL_BASE=/path/to/base_llm_model
 TARGET_DIR=bunny-lora-phi-2
 
-python -m bunny.eval.eval_spatialqa \
+python -m bunny.eval.eval_spatialbench \
     --model-path ./checkpoints-$MODEL_TYPE/$TARGET_DIR\
     --model-base $MODEL_BASE\
     --model-type $MODEL_TYPE\
-    --data-path ./eval/spatialqa_bench \
+    --data-path ./eval/spatial_bench \
     --conv-mode bunny \
-    --question existence.json \
+    --question size.json \
     --depth
