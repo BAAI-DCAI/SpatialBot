160c0c185c4d93df76cc1a3264db33cb1eff898b
WenxiaoCai <robincwx@foxmail.com>
2024-09-18 21:52:42 -0700

    Fixed a bug in spatialbench.sh

--- script/eval/full/spatial_bench.sh
+++ script/eval/full/spatial_bench.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 MODEL_TYPE=phi-2
 TARGET_DIR=bunny-phi-2
 
 python -m bunny.eval.eval_spatialbench \
+    --model-path ./checkpoints-$MODEL_TYPE/$TARGET_DIR \
     --model-type $MODEL_TYPE\
     --data-path ./eval/spatial_bench \
     --conv-mode bunny \
